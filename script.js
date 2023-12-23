af97663425c6c8f5c6b1ef85137af75d1b15b7f5
al mamun <almamun17693@gmail.com>
2023-12-23 23:03:17 +0600

    add sometext

---
+++ script.js
@@ -0,0 +1,42 @@
+// let poorCountry = ['bangladesh', 'srilanka', 'napal','bhutan']
+// let richCountry = [...poorCountry,'usa', 'canada', 'franch', 'germany']
+
+// console.log(richCountry);
+// let poorCountry = ['bangladesh', 'srilanka', 'napal','bhutan']
+// let richCountry = ['usa', 'canada', 'franch', 'germany']
+
+// richCountry.push(poorCountry)
+
+// console.log(richCountry);
+
+
+
+// function show(a, b,...numbers) {
+//     let sum = 0;
+//     for (let i of numbers) {
+//         sum = sum + i
+        
+//     }
+//  console.log (sum)
+// }
+
+// show(10, 30, 5, 7, 3);
+
+
+// var a = 25;
+// var a = 30;
+
+// console.log(a);
+
+
+
+
+var i;
+for (i = 2; i < 100; i = i+2) {
+    console.log(i);
+}
+
+var i;
+for (i = 1; i < 100; i = i+2) {
+    console.log(i);
+}
